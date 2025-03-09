342c253eb668dc0b011d899a2f1f8915e69410d0
Torrenté Florian <lukyrouge4@gmail.com>
2025-03-09 08:52:34 +0100

    Renamed files, implemented /deploy endpoint

--- contracts/src/backend.ts
+++ contracts/src/backend.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+//@ts-ignore
 import bodyParser from 'body-parser';
 import { WordCommitment } from './WordCommitment.js';
 import { GuessVerifier } from './GuessVerifier.js';
@@ -21,31 +22,39 @@ const deployerAccount = Local.testAccounts[0];
 const deployerKey = deployerAccount.key;
 const senderAccount = Local.testAccounts[1];
 
-// Generate random salt
-const salt = Field.random();
-
 // Create zkApp destination (zkApp address and private key)
 const zkAppPrivateKey = PrivateKey.random();
 const zkAppAddress = zkAppPrivateKey.toPublicKey();
 const zkAppInstance = new WordCommitment(zkAppAddress);
 
-// Deploy zkApp and initialize it with a word hash (e.g., "Apple")
-const deployTxn = await Mina.transaction(deployerAccount, async () => {
-  AccountUpdate.fundNewAccount(deployerAccount);
-  await zkAppInstance.deploy();
-  await zkAppInstance.initState(CircuitString.fromString("Apple"));
-});
-await deployTxn.prove();
-await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
+app.post('/deploy', async (req: Request, res: any) => {
+  const { word } = req.body;
+  if (!word) {
+    return res.status(400).json({ error: 'No word provided' });
+  }
 
-console.log("Contract deployed and initialized.");
+  try {
+    const deployTxn = await Mina.transaction(deployerAccount, async () => {
+      AccountUpdate.fundNewAccount(deployerAccount);
+      await zkAppInstance.deploy();
+      await zkAppInstance.initState(CircuitString.fromString(word));
+    });
+    await deployTxn.prove();
+    await deployTxn.sign([deployerKey, zkAppPrivateKey]).send();
+
+    res.status(200).json({ message: 'Contract deployed and initialized.' });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: 'Failed to deploy contract' });
+  }
+});
 
 // REST endpoint to verify a guess
 app.post('/verify-guess', async (req: Request, res: any) => {
   const { guess } = req.body;
 
   if (!guess) {
-    return res.status(400).json({ error: "No guess provided" });
+    return res.status(400).json({ error: 'No guess provided' });
   }
 
   try {
@@ -57,10 +66,12 @@ app.post('/verify-guess', async (req: Request, res: any) => {
 
     console.log(`✅ Guess "${guess}" is correct!`);
 
-    return res.status(200).json({ message: `✅ Guess "${guess}" is correct!`});
+    return res.status(200).json({ message: `✅ Guess "${guess}" is correct!` });
   } catch (error) {
     console.log(`❌ Guess "${guess}" is incorrect.`);
-    return res.status(400).json({ message: `❌ Guess "${guess}" is incorrect.`});
+    return res
+      .status(400)
+      .json({ message: `❌ Guess "${guess}" is incorrect.` });
   }
 });
 
